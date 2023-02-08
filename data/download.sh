2d10eaf050152aac2f2415377d93b2eac7504738
nsohail19 <koheenur@gmail.com>
2023-02-07 20:42:32 -0500

    Script to download example website from 10x

---
+++ data/download.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+wget https://cf.10xgenomics.com/samples/cell-exp/3.1.0/5k_pbmc_protein_v3/5k_pbmc_protein_v3_filtered_feature_bc_matrix.h5 .
+wget https://cf.10xgenomics.com/samples/cell-exp/3.1.0/5k_pbmc_protein_v3_nextgem/5k_pbmc_protein_v3_nextgem_filtered_feature_bc_matrix.h5 .
